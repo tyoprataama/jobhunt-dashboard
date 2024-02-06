803074602d04224e6369c166b9e185e6fdffdc4f
tyoprataama <tyoprataama004@gmail.com>
2024-02-06 22:52:17 +0700

    build: implementing useRoute

---
+++ src/app/create-job/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const page = () => {
+  return <div>Post a job</div>;
+};
+
+page.propTypes = {};
+
+export default page;
--- src/components/layout/Mainbar/index.tsx
+++ src/components/layout/Mainbar/index.tsx
@@ -1,25 +1,23 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
 import React, { FC } from "react";
+import { useRouter } from "next/navigation";
 
 interface indexProps {}
 const Mainbar: FC<indexProps> = () => {
+  const router = useRouter();
+  const navLink = () => router.push('/create-job')
   return (
-    <div className="py-4 px-6 min-h-screen">
+    <div className="py-4 px-6">
       <div className="flex justify-between border-b">
         <div className="flex flex-col">
           <h1 className="text-lg">Company</h1>
           <h2 className="text-base font-semibold pb-4">Twitter</h2>
         </div>
-        <Button className="bg-indigo-700 rounded-none py-3 px-6">
+        <Button onClick={navLink} className="bg-indigo-700 rounded-none py-3 px-6">
           <PlusIcon className="mr-2 w-4 h-4" /> Post a Job
         </Button>
-      </div>
-      <div className="py-6">
-        <h1 className="text-2xl font-semibold">Good Morning, Charles</h1>
-        <p className="text-slate-400 font-semibold">
-          Here is your job listing statistic report from July 19 - July 25.
-        </p>
       </div>
     </div>
   );
